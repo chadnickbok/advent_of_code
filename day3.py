63b23395361fc47130e4f3f5e7bb0c0dd66f931a
nick <chadnickbok@gmail.com>
2020-12-02 21:35:01 -0800

    day3 pt2

--- day3.py
+++ day3.py
@@ -5,20 +5,34 @@ import sys
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-
 print(len(lines[0].strip()))
 print(lines[0])
 
-pos = 0
-trees = 0
-for line in lines:
-    cur_line = line.strip()
-    if pos >= len(cur_line):
-        pos = pos % len(cur_line)
 
-    if cur_line[pos] is "#":
-        trees += 1
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
 
-    pos += 3
+tree_counts = []
 
-print(trees)
+for slope in slopes:
+    pos = 0
+    trees = 0
+    i = 0
+    while i < len(lines):
+        cur_line = lines[i].strip()
+        if pos >= len(cur_line):
+            pos = pos % len(cur_line)
+
+        if cur_line[pos] is "#":
+            trees += 1
+
+        pos += slope[0]
+        i += slope[1]
+    print(trees)
+    tree_counts.append(trees)
+
